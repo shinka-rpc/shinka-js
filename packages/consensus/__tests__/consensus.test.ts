35f3c79e049f765779db03e90bc71f128a805365
Anton Kovalevich <kai3341@gmail.com>
2026-07-09 23:49:43 +0200

    compress test `consensus`

--- packages/consensus/__tests__/consensus.test.ts
+++ packages/consensus/__tests__/consensus.test.ts
@@ -13,13 +13,21 @@ const testPair = (a: number, b: number) => {
   expect(consensus(b, a)).toStrictEqual(pairMap.get(consensusResult));
 };
 
-const testRange = (x: number, y: number) => {
-  for (let a = x; a < y; a++) for (let b = x; b < y; b++) testPair(a, b);
+const testRange = (a1: number, a2: number, b1: number, b2: number) => {
+  for (let a = a1; a < a2; a++) for (let b = b1; b < b2; b++) testPair(a, b);
 };
 
-const test100 = (x: number) => testRange(x, x + 100);
+const testN = (N: number, a: number, b: number) =>
+  testRange(a, a + N, b, b + N);
 
-test("consensus-exact", () => {
+const runTest =
+  (N: number) =>
+  ([a, b]: [number, number]) =>
+    test(`consensus-range-${N}-${a}-${b}`, () => testN(N, a, b));
+
+// ===
+
+test("consensus-exact-1-2", () => {
   expect(1).toStrictEqual(1);
   const a = 1;
   const b = 2;
@@ -27,11 +35,14 @@ test("consensus-exact", () => {
   expect(consensus(b, a)).toStrictEqual(Consensus.FAIL);
 });
 
-test("consensus-range-0-100", () => test100(0));
-test("consensus-range-2000-2100", () => test100(2000));
-
-// chosen by fair dice roll
-test("consensus-range-7116-7216", () => test100(7116));
-test("consensus-range-12762-12862", () => test100(12762));
-test("consensus-range-27554-27654", () => test100(27554));
-test("consensus-range-32129-32229", () => test100(32129));
+(
+  [
+    [0, 0],
+    [2000, 2000],
+    // chosen by fair dice roll
+    [7116, 32129],
+    [27554, 12762],
+    [2881, 7265],
+    [29987, 25308],
+  ] as [number, number][]
+).forEach(runTest(100));
